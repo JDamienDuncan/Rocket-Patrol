5b643454dbe0d634c50dba79a89bd2e640261fbd
John Duncan <jdduncan@ucsc.edu>
2020-04-13 19:41:37 -0700

    Ready for Submit

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,40 +1,38 @@
 // Rocket prefab
-class Rocket extends Phaser.GameObjects.Sprite{
-    constructor(scene, x, y, texture, frame){
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
-
-        // Add object to existing scene
-        scene.add.existing(this); // Add to existing, displayList, updateList
-        this.isFiring = false;    // Track rocket's firing status
-
+        scene.add.existing(this);   // add to existing scene, displayList, updateList
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
+        this.isFiring = false;      // track firing status
     }
 
     update() {
-
-        if(!this.isFiring) {
-            if(keyLEFT.isDown && this.x >= 47) {
+        // left/right movement
+        if (!this.isFiring) {
+            if (keyLEFT.isDown && this.x >= 47) {
                 this.x -= 2;
-            } else if (keyRIGHT.isDown && this.x <= 578) {
+            } else if (keyRIGHT.isDown && this.x <= 598) {
                 this.x += 2;
             }
         }
-        // Fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        // fire button
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
-
+            this.sfxRocket.play();  // play sfx
         }
-
-        if(this.isFiring && this.y >= 108){
+        // if fired, move up
+        if (this.isFiring && this.y >= 108) {
             this.y -= 2;
         }
-
-        if(this.y <= 108){
-            this.isFiring = false;
-            this.y = 431;
+        // reset on miss
+        if (this.y <= 108) {
+            this.reset();
         }
-
-
-    
     }
-
+    // reset rocket to "ground"
+    reset() {
+        this.isFiring = false;
+        this.y = 431;
+    }
 }
--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,23 +1,20 @@
-class Spaceship extends Phaser.GameObjects.Sprite{
-    constructor(scene, x, y, texture, frame, pointValue){
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
         super(scene, x, y, texture, frame);
-
-        // Add object to existing scene
-        scene.add.existing(this); // Add to existing, displayList, updateList
-        this.points = pointValue;
-
+        scene.add.existing(this);   // add to existing scene, displayList, updateList
+        this.points = pointValue;   // store pointValue
     }
 
     update() {
-        // Move spaceship left
-        this.x -= 3;
-        // Wraparound screen bounds
-        if(this.x <= 0 - this.width) {
-            this.x = game.config.width;
-
+        // move spaceship left
+        this.x -= 4;
+        // wraparound from left to right edge
+        if (this.x <= 0-this.width) {
+            this.reset();
         }
-
-    
     }
 
+    reset() {
+        this.x = game.config.width;
+    }
 }
